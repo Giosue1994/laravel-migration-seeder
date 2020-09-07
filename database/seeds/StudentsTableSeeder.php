16be738f73ecc928c38d668874d746ebec48761b
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-07 16:59:42 +0200

    fakers

---
+++ database/seeds/StudentsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Student;
+
+class StudentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) {
+          $new_student = new Student();
+          $new_student->name = $faker->name;
+          $new_student->lastname = $faker->lastName;
+          $new_student->age = $faker->numberBetween(18, 50);
+          $new_student->vote = $faker->numberBetween(0, 10);
+          $new_student->note = $faker->paragraph;
+          $new_student->save();
+        }
+    }
+}
