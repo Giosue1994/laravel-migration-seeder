e688efff9abc132a2fae50429375d10afe2a1687
Giosue1994 <giosuelentini1994@outlook.com>
2020-09-07 17:50:10 +0200

    delete

--- resources/views/classes/students.blade.php
+++ resources/views/classes/students.blade.php
@@ -6,6 +6,12 @@
     <li><b>Cognome: {{ $student->lastname }}</b></li>
 
     <p><a href="{{ route('students.show', $student) }}">Mostra dettagli</a></p>
+    <form action="{{ route('students.destroy', $student) }}" method="post">
+      @csrf
+      @method('DELETE')
+
+      <input type="submit" value="Elimina">
+    </form>
     <hr>
   @endforeach
 </ul>
